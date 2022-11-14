ae57f6c6fad74e8682b82eba9eab5b2c6f62cc71
xinghe <xinghe2@h-partners.com>
2022-11-14 11:42:32 +0000

    upgrade to 5.7

--- cache_swap.sh
+++ cache_swap.sh
@@ -5,12 +5,17 @@ fi
 
 SQUID_CONF=${SQUID_CONF:-"/etc/squid/squid.conf"}
 
-CACHE_SWAP=`sed -e 's/#.*//g' $SQUID_CONF | \
-	grep cache_dir | awk '{ print $3 }'`
+CACHE_SWAP=`awk '/^[[:blank:]]*cache_dir/ { print $3 }' "$SQUID_CONF"`
 
+init_cache_dirs=0
 for adir in $CACHE_SWAP; do
 	if [ ! -d $adir/00 ]; then
 		echo -n "init_cache_dir $adir... "
-		squid -N -z -F -f $SQUID_CONF >> /var/log/squid/squid.out 2>&1
+		init_cache_dirs=1
 	fi
 done
+
+if [ $init_cache_dirs -ne 0 ]; then
+	echo ""
+	squid --foreground -z -f "$SQUID_CONF" >> /var/log/squid/squid.out 2>&1
+fi
